e10661f934a6849a9c84c62f23e940afb31c87db
xonix <xonixx@gmail.com>
2021-12-02 20:34:25 +0200

    tests

---
+++ src/test/java/gc_less/LongStackTests.java
@@ -0,0 +1,45 @@
+package gc_less;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LongStackTests {
+  @Test
+  public void test1() {
+    long stack = LongStack.init(10);
+
+    assertEquals(0, LongStack.getLength(stack));
+
+    stack = LongStack.push(stack, 111);
+
+    assertEquals(1, LongStack.getLength(stack));
+
+    stack = LongStack.push(stack, 222);
+
+    assertEquals(2, LongStack.getLength(stack));
+    assertEquals(222, LongStack.peek(stack));
+    assertEquals(222, LongStack.pop(stack));
+    assertEquals(1, LongStack.getLength(stack));
+    assertEquals(111, LongStack.pop(stack));
+    assertEquals(0, LongStack.getLength(stack));
+
+    LongStack.free(stack);
+  }
+
+  @Test
+  public void test2() {
+    long stack = LongStack.init(2);
+
+    for (int i = 0; i < 10; i++) {
+      stack = LongStack.push(stack, i);
+    }
+
+    assertEquals(10, LongStack.getLength(stack));
+    assertTrue(LongStack.getCapacity(stack) >= 10);
+    assertEquals(9, LongStack.peek(stack));
+
+    LongStack.free(stack);
+  }
+}
