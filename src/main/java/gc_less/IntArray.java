944e8ceca0544151fc8ff0effb6b458170263bdd
xonix <xonixx@gmail.com>
2021-12-04 15:47:32 +0200

    IntArray : checkBoundaries

--- src/main/java/gc_less/IntArray.java
+++ src/main/java/gc_less/IntArray.java
@@ -4,7 +4,6 @@ import static gc_less.TypeSizes.INT_SIZE;
 import static gc_less.Unsafer.getUnsafe;
 
 /** Non-resizable array (similar to arrays in Java) */
-// TODO check boundaries
 // TODO arraycopy
 public class IntArray {
   private static final long lengthOffset = 0;
@@ -19,11 +18,17 @@ public class IntArray {
   }
 
   public static void set(long address, long index, int value) {
+    checkBoundaries(address, index);
     getUnsafe().putInt(address + dataOffset + index * INT_SIZE, value);
   }
 
   public static int get(long address, long index) {
+    checkBoundaries(address, index);
     return getUnsafe().getInt(address + dataOffset + index * INT_SIZE);
+  }
+
+  private static void checkBoundaries(long address, long index) {
+    if (index < 0 || index >= getLength(address)) throw new IndexOutOfBoundsException();
   }
 
   public static int getLength(long address) {
--- src/test/java/gc_less/IntArrayTests.java
+++ src/test/java/gc_less/IntArrayTests.java
@@ -2,7 +2,7 @@ package gc_less;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class IntArrayTests {
   @Test
@@ -12,5 +12,34 @@ public class IntArrayTests {
     assertEquals(0, IntArray.get(array, 0));
     assertEquals(0, IntArray.get(array, 7));
     assertEquals(0, IntArray.get(array, 9));
+    IntArray.free(array);
+  }
+
+  @Test
+  public void testLegalAccess() {
+    long array = IntArray.allocate(10);
+    IntArray.set(array, 0, 111);
+    IntArray.set(array, 7, 222);
+    IntArray.set(array, 9, 333);
+    assertEquals(111, IntArray.get(array, 0));
+    assertEquals(222, IntArray.get(array, 7));
+    assertEquals(333, IntArray.get(array, 9));
+    IntArray.free(array);
+  }
+
+  @Test
+  public void testIllegalAccess() {
+    long array = IntArray.allocate(10);
+
+    assertThrows(IndexOutOfBoundsException.class, () -> Integer.hashCode(IntArray.get(array, -1)));
+    assertThrows(IndexOutOfBoundsException.class, () -> IntArray.set(array, -1, 7));
+
+    assertDoesNotThrow(() -> Integer.hashCode(IntArray.get(array, 5)));
+    assertDoesNotThrow(() -> IntArray.set(array, 5, 7));
+
+    assertThrows(IndexOutOfBoundsException.class, () -> Integer.hashCode(IntArray.get(array, 10)));
+    assertThrows(IndexOutOfBoundsException.class, () -> IntArray.set(array, -1, 10));
+
+    IntArray.free(array);
   }
 }
