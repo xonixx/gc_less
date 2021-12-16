867a4c8306ff46cdf5e824ac061005bbcdb898fa
xonix <xonixx@gmail.com>
2021-12-16 17:12:16 +0200

    TemplateHashtable: tests

--- src/test/java/gc_less/tpl/TemplateHashtableTests.java
+++ src/test/java/gc_less/tpl/TemplateHashtableTests.java
@@ -9,14 +9,39 @@ public class TemplateHashtableTests {
   @Test
   public void testCreate() {
     try (Allocator allocator = Allocator.newFrame()) {
+      // GIVEN
       long hashTable = TemplateHashtable.allocate(allocator, 10, .75f);
+      // WHEN
       hashTable = TemplateHashtable.put(hashTable, 1, 111);
       hashTable = TemplateHashtable.put(hashTable, 2, 222);
       hashTable = TemplateHashtable.put(hashTable, 3, 333);
       hashTable = TemplateHashtable.put(hashTable, 3, 444);
       hashTable = TemplateHashtable.put(hashTable, 13, 4444);
       System.out.println(TemplateHashtable.toString(hashTable));
+      // THEN
       assertEquals(4, TemplateHashtable.getSize(hashTable));
+    }
+  }
+  @Test
+  public void testGet() {
+    try (Allocator allocator = Allocator.newFrame()) {
+      // GIVEN
+      long hashTable = TemplateHashtable.allocate(allocator, 10, .75f);
+      // WHEN
+      hashTable = TemplateHashtable.put(hashTable, 1, 111);
+      hashTable = TemplateHashtable.put(hashTable, 2, 222);
+      hashTable = TemplateHashtable.put(hashTable, 3, 333);
+      hashTable = TemplateHashtable.put(hashTable, 3, 444);
+      hashTable = TemplateHashtable.put(hashTable, 13, 4444);
+      System.out.println(TemplateHashtable.toString(hashTable));
+      // THEN
+      assertEquals(0, TemplateHashtable.get(hashTable, 0));
+      assertEquals(0, TemplateHashtable.get(hashTable, 100));
+      assertEquals(111, TemplateHashtable.get(hashTable, 1));
+      assertEquals(222, TemplateHashtable.get(hashTable, 2));
+      assertEquals(444, TemplateHashtable.get(hashTable, 3));
+      assertEquals(4444, TemplateHashtable.get(hashTable, 13));
+      assertEquals(0, TemplateHashtable.get(hashTable, 14));
     }
   }
 }
