d51fbb4b25ec3eb097facb5cf6368a60b5434a42
xonix <xonixx@gmail.com>
2021-12-16 17:06:55 +0200

    TemplateHashtable

--- src/test/java/gc_less/tpl/TemplateHashtableTests.java
+++ src/test/java/gc_less/tpl/TemplateHashtableTests.java
@@ -10,7 +10,13 @@ public class TemplateHashtableTests {
   public void testCreate() {
     try (Allocator allocator = Allocator.newFrame()) {
       long hashTable = TemplateHashtable.allocate(allocator, 10, .75f);
+      hashTable = TemplateHashtable.put(hashTable, 1, 111);
+      hashTable = TemplateHashtable.put(hashTable, 2, 222);
+      hashTable = TemplateHashtable.put(hashTable, 3, 333);
+      hashTable = TemplateHashtable.put(hashTable, 3, 444);
+      hashTable = TemplateHashtable.put(hashTable, 13, 4444);
       System.out.println(TemplateHashtable.toString(hashTable));
+      assertEquals(4, TemplateHashtable.getSize(hashTable));
     }
   }
 }
