361fa21302b61387a139af6a73ce3e531c0f4f8c
xonix <xonixx@gmail.com>
2024-01-22 02:14:12 +0200

    malloc / realloc / free in java 23

--- src/main/java/gc_less/no_unsafe/Main2.java
+++ src/main/java/gc_less/no_unsafe/Main2.java
@@ -1,18 +1,17 @@
 package gc_less.no_unsafe;
 
 import java.lang.foreign.*;
-import java.lang.invoke.MethodHandle;
-import java.util.function.Consumer;
 
 public class Main2 {
   public static void main(String[] args) throws Throwable {
     MemorySegment p1 = NativeMem.malloc(500_000_000L);
     p1.fill((byte) 7);
+    System.out.println("p1=" + p1.address());
     System.out.println(p1.byteSize());
-
     Thread.sleep(5000);
 
     MemorySegment p2 = NativeMem.realloc(p1, 1000_000_000L);
+    System.out.println("p2=" + p2.address());
     System.out.println(p2.byteSize());
     System.out.println(p2.get(ValueLayout.JAVA_BYTE, 0)); // should be 7
     p2.fill((byte) 8);
