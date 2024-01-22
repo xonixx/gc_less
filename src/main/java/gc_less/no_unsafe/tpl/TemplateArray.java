21c132bd69c90983b38bf827bbb79399f3c2319c
xonix <xonixx@gmail.com>
2024-01-22 22:27:23 +0200

    bootstrap TemplateArray.java

---
+++ src/main/java/gc_less/no_unsafe/tpl/TemplateArray.java
@@ -0,0 +1,64 @@
+package gc_less.no_unsafe.tpl;
+
+import static gc_less.TypeSizes.INT_SIZE;
+import static gc_less.Unsafer.getUnsafe;
+
+import gc_less.Allocator;
+import gc_less.Ref;
+import gc_less.tpl.Tpl;
+import gc_less.tpl.Type;
+
+/** Non-resizable array (similar to arrays in Java) */
+public class TemplateArray {
+  private static final long lengthOffset = 0;
+  private static final long dataOffset = lengthOffset + INT_SIZE;
+
+  public static long allocate(int length) {
+    return allocate(null, length);
+  }
+
+  public static long allocate(Allocator allocator, int length) {
+    long bytes = dataOffset + length * Tpl.typeSize();
+    long addr = getUnsafe().allocateMemory(bytes);
+    getUnsafe().setMemory(addr, bytes, (byte) 0);
+    setLength(addr, length);
+    if (allocator != null) {
+      allocator.registerForCleanup(Ref.create(addr));
+    }
+    return addr;
+  }
+
+  public static void set(long address, int index, @Type long value) {
+    checkBoundaries(address, index);
+    Tpl.put(address + dataOffset + index * Tpl.typeSize(), value);
+  }
+
+  public static @Type long get(long address, int index) {
+    checkBoundaries(address, index);
+    return Tpl.get(address + dataOffset + index * Tpl.typeSize());
+  }
+
+  private static void checkBoundaries(long address, int index) {
+    if (index < 0 || index >= getLength(address)) throw new IndexOutOfBoundsException();
+  }
+
+  public static int getLength(long address) {
+    return getUnsafe().getInt(address);
+  }
+
+  private static void setLength(long address, int length) {
+    getUnsafe().putInt(address, length);
+  }
+
+  public static void free(long address) {
+    getUnsafe().freeMemory(address);
+  }
+
+  public static void arraycopy(long src, int srcPos, long dest, int destPos, int length) {
+    getUnsafe()
+        .copyMemory(
+            src + dataOffset + srcPos * Tpl.typeSize(),
+            dest + dataOffset + destPos * Tpl.typeSize(),
+            length * Tpl.typeSize());
+  }
+}
