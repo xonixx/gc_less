daaea32e7f283f2ded2b4983170f3ff856ae66c1
xonix <xonixx@gmail.com>
2024-02-09 14:00:40 +0200

    Consider implementing hashtable the same as in Python #5 IntHashtableOffHeap.java impr

--- src/main/java/gc_less/python_like/IntHashtableOffHeap.java
+++ src/main/java/gc_less/python_like/IntHashtableOffHeap.java
@@ -42,10 +42,12 @@ public class IntHashtableOffHeap {
 
     int prevVal = 0;
 
-    int storageLen = (int) storage.byteSize();
-    int delta = 2 * intSize;
-
-    for (int bucketIdxOffset = bucket * delta, i = 0; /*i<capacity*/ ; i++, bucketIdxOffset += delta) {
+    for (int storageLen = (int) storage.byteSize(),
+            delta = 2 * intSize,
+            bucketIdxOffset = bucket * delta,
+            i = 0; /*i<capacity*/
+        ;
+        i++, bucketIdxOffset += delta) {
       if (bucketIdxOffset >= storageLen) {
         bucketIdxOffset = 0;
       }
@@ -71,10 +73,12 @@ public class IntHashtableOffHeap {
     // we utilize the fact that for int : hash = value
     int bucket = key % capacity;
 
-    int storageLen = (int) storage.byteSize();
-    int delta = 2 * intSize;
-
-    for (int bucketIdxOffset = bucket * delta, i = 0; /*i<capacity*/ ; i++, bucketIdxOffset += delta) {
+    for (int storageLen = (int) storage.byteSize(),
+            delta = 2 * intSize,
+            bucketIdxOffset = bucket * delta,
+            i = 0; /*i<capacity*/
+        ;
+        i++, bucketIdxOffset += delta) {
       if (bucketIdxOffset >= storageLen) {
         bucketIdxOffset = 0;
       }
@@ -103,10 +107,9 @@ public class IntHashtableOffHeap {
       storage = NativeMem.malloc((long) capacity * intSize * 2);
       storage.fill((byte) 0);
 
-      int storageLen = (int) oldStorage.byteSize();
-      int delta = 2 * intSize;
-
-      for (int bucketIdxOffset = 0; bucketIdxOffset < storageLen; bucketIdxOffset += delta) {
+      for (int storageLen = (int) oldStorage.byteSize(), delta = 2 * intSize, bucketIdxOffset = 0;
+          bucketIdxOffset < storageLen;
+          bucketIdxOffset += delta) {
         int key = oldStorage.get(JAVA_INT, bucketIdxOffset);
         if (key != 0) {
           insert(key, oldStorage.get(JAVA_INT, bucketIdxOffset + intSize));
@@ -120,10 +123,12 @@ public class IntHashtableOffHeap {
     int bucket = key % capacity;
     // we utilize the fact that for int : hash = value
 
-    int storageLen = (int) storage.byteSize();
-    int delta = 2 * intSize;
-
-    for (int bucketIdxOffset = bucket * delta, i = 0; /*i<capacity*/ ; i++, bucketIdxOffset += delta) {
+    for (int storageLen = (int) storage.byteSize(),
+            delta = 2 * intSize,
+            bucketIdxOffset = bucket * delta,
+            i = 0; /*i<capacity*/
+        ;
+        i++, bucketIdxOffset += delta) {
       if (bucketIdxOffset >= storageLen) {
         bucketIdxOffset = 0;
       }
