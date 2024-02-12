8fa1fa5858b85ad794c85cf284ffbbbfac3af975
xonix <xonixx@gmail.com>
2024-02-13 00:50:58 +0200

    access speed test : write speed

--- src/main/java/gc_less/no_unsafe/AccessSpeedTest.java
+++ src/main/java/gc_less/no_unsafe/AccessSpeedTest.java
@@ -9,14 +9,15 @@ import java.lang.foreign.MemorySegment;
 
 public class AccessSpeedTest {
 
-//  public static final int LEN = 20_000_000;
+  //  public static final int LEN = 20_000_000;
   public static final int LEN = 10_000_000;
-//  public static final int TIMES = 200;
-//  public static final int TIMES = 500;
-  public static final int TIMES = 100;
+    public static final int TIMES = 200;
+  //  public static final int TIMES = 500;
+//  public static final int TIMES = 100;
 
   public static void main(String[] args) {
     testRead();
+    testWrite();
   }
 
   private static void testRead() {
@@ -46,9 +47,44 @@ public class AccessSpeedTest {
     long arrTime = t3 - t2;
     System.out.println("arr: " + arrTime);
 
+    System.out.println("\n--- READ SPEED ---");
     System.out.println("arr faster than memSegm   : " + (1f * memTime / arrTime) + "x");
     System.out.println("arr faster than Unsafe    : " + (1f * unsTime / arrTime) + "x");
-    System.out.println("memSegm slower than Unsafe: " + (1f * memTime / unsTime) + "x");
+    System.out.println("memSegm faster than Unsafe: " + (1f * unsTime / memTime) + "x");
+    System.out.println();
+  }
+  private static void testWrite() {
+    long t0 = System.currentTimeMillis();
+
+    int res1 = testMemWrite();
+    System.out.println("res1=" + res1);
+
+    long t1 = System.currentTimeMillis();
+
+    int res2 = testUnsafeWrite();
+    System.out.println("res2=" + res2);
+
+    long t2 = System.currentTimeMillis();
+
+    int res3 = testArrWrite();
+    System.out.println("res3=" + res3);
+
+    long t3 = System.currentTimeMillis();
+
+    long memTime = t1 - t0;
+    System.out.println("mem: " + memTime);
+
+    long unsTime = t2 - t1;
+    System.out.println("uns: " + unsTime);
+
+    long arrTime = t3 - t2;
+    System.out.println("arr: " + arrTime);
+
+    System.out.println("\n--- WRITE SPEED ---");
+    System.out.println("arr faster than memSegm   : " + (1f * memTime / arrTime) + "x");
+    System.out.println("arr faster than Unsafe    : " + (1f * unsTime / arrTime) + "x");
+    System.out.println("memSegm faster than Unsafe: " + (1f * unsTime / memTime) + "x");
+    System.out.println();
   }
 
   private static int testMemRead() {
@@ -113,6 +149,70 @@ public class AccessSpeedTest {
       for (int j = LEN; j-- > 0; ) {
         res += arr[j];
       }
+    }
+
+    return res;
+  }
+
+  private static int testMemWrite() {
+    MemorySegment mem = NativeMem.malloc(LEN * 4);
+
+    // write
+    for (int i = 0; i < TIMES; i++) {
+      for (int j = 0; j < LEN; j++) {
+        mem.set(JAVA_INT, j * 4, i);
+      }
+      for (int j = LEN; j-- > 0; ) {
+        mem.set(JAVA_INT, j * 4, i);
+      }
+    }
+
+    int res = 0;
+    for (int i = 0; i < LEN; i++) {
+      res += mem.get(JAVA_INT, i * 4);
+    }
+
+    return res;
+  }
+
+  private static int testUnsafeWrite() {
+    Unsafe unsafe = Unsafer.getUnsafe();
+    long mem = unsafe.allocateMemory(LEN * 4);
+
+    // write
+    for (int i = 0; i < TIMES; i++) {
+      for (int j = 0; j < LEN; j++) {
+        unsafe.putInt(mem + j * 4, i);
+      }
+      for (int j = LEN; j-- > 0; ) {
+        unsafe.putInt(mem + j * 4, i);
+      }
+    }
+
+    int res = 0;
+    for (int i = 0; i < LEN; i++) {
+      res += unsafe.getInt(mem + i * 4);
+    }
+
+    return res;
+  }
+
+  private static int testArrWrite() {
+    int[] arr = new int[LEN];
+
+    // write
+    for (int i = 0; i < TIMES; i++) {
+      for (int j = 0; j < LEN; j++) {
+        arr[j] = i;
+      }
+      for (int j = LEN; j-- > 0; ) {
+        arr[j] = i;
+      }
+    }
+
+    int res = 0;
+    for (int i = 0; i < LEN; i++) {
+      res += arr[i];
     }
 
     return res;
