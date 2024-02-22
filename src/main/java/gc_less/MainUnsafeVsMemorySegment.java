b9d316ed128d5af602ceeda6a70c998485df8a4c
xonix <xonixx@gmail.com>
2024-02-23 01:54:42 +0200

    MainUnsafeVsMemorySegment.java

---
+++ src/main/java/gc_less/MainUnsafeVsMemorySegment.java
@@ -0,0 +1,32 @@
+package gc_less;
+
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
+
+public class MainUnsafeVsMemorySegment {
+  public static final int N = 1000_000;
+
+  public static void main(String[] args) {
+    System.out.println("Unsafe-based hashtable");
+    try (Cleaner cleaner = new Cleaner()) {
+      long hashtableUnsafe = IntHashtable.allocate(cleaner, 100, .75f);
+      for (int i = 0; i < N; i++) {
+        if (i % 1000 == 0) {
+          System.out.println("Unsafe-based: " + i);
+        }
+        hashtableUnsafe = IntHashtable.put(hashtableUnsafe, i, i);
+      }
+    }
+
+    System.out.println("MemorySegment-based hashtable");
+    try (Arena arena = Arena.ofShared()) {
+      MemorySegment hashtableMS = gc_less.no_unsafe.IntHashtable.allocate(arena, 100, .75f);
+      for (int i = 0; i < N; i++) {
+        if (i % 1000 == 0) {
+          System.out.println("MemorySegment-based: " + i);
+        }
+        hashtableMS = gc_less.no_unsafe.IntHashtable.put(hashtableMS, i, i);
+      }
+    }
+  }
+}
