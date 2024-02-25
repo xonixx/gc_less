c7903b584e84f4538b076d34efdc03e88ba0e1cb
xonix <xonixx@gmail.com>
2024-02-26 00:47:35 +0200

    mem segm memory consumption

--- src/main/java/gc_less/MainHashtableComparison.java
+++ src/main/java/gc_less/MainHashtableComparison.java
@@ -5,14 +5,14 @@ import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
 
 public class MainHashtableComparison {
-  public static final int N = 1000_000;
+  public static final int N = 50_000_000;
 
   public static void main(String[] args) {
     System.out.println("\n===== Unsafe-based hashtable =====");
     try (Cleaner cleaner = new Cleaner()) {
       long hashtableUnsafe = IntHashtable.allocate(cleaner, 100, .75f);
       for (int i = 0; i < N; i++) {
-        if (i % 1000 == 0) {
+        if (i % 10000 == 0) {
           System.out.println("Unsafe-based: " + i);
         }
         hashtableUnsafe = IntHashtable.put(hashtableUnsafe, i, i);
@@ -22,7 +22,7 @@ public class MainHashtableComparison {
     System.out.println("\n===== Python-based hashtable =====");
     IntHashtableOffHeap intHashtablePy = new IntHashtableOffHeap(100, .75f);
     for (int i = 0; i < N; i++) {
-      if (i % 1000 == 0) {
+      if (i % 10000 == 0) {
         System.out.println("Python-based: " + i);
       }
       intHashtablePy.put(i, i);
@@ -33,7 +33,7 @@ public class MainHashtableComparison {
     try (Arena arena = Arena.ofShared()) {
       MemorySegment hashtableMS = gc_less.no_unsafe.IntHashtable.allocate(arena, 100, .75f);
       for (int i = 0; i < N; i++) {
-        if (i % 1000 == 0) {
+        if (i % 10000 == 0) {
           System.out.println("MemorySegment-based: " + i);
         }
         hashtableMS = gc_less.no_unsafe.IntHashtable.put(hashtableMS, i, i);
